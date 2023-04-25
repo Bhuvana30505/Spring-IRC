aa7ff8339fd55ec45881ca620d7cb5a59be1de41
Bhuvana30505 <bhuvana30505@gmail.com>
2023-04-25 10:30:36 +0530

    day1

--- day3/Day4-2/src/main/java/projectmodel/kabbadi.java
+++ day3/Day4-2/src/main/java/projectmodel/kabbadi.java
@@ -1,13 +1,15 @@
 package projectmodel;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="Kabbadi")
-public class Kabbadi {
+public class kabbadi{
 	@Id
+	@GeneratedValue
     private int id;
 	private String name;
 	private  int age;
